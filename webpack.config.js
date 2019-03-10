15e7c7504c6ff5da394d652d473e17326bd52f6c
Anton Ovechkin <owanturist@gmail.com>
2019-03-10 11:43:26 +0700

    Made prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,112 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-    entry: [
-        'normalize.css',
-        path.resolve('./src/styles.pcss'),
-        path.resolve('./src/app.js')
-    ],
+module.exports = _env => {
+    const env = _env || {};
 
-    output: {
-        path: path.resolve('./build'),
-        filename: '[name]-[hash].js'
-    },
+    return {
+        entry: [
+            'normalize.css',
+            path.resolve('./src/styles.pcss'),
+            path.resolve('./src/app.js')
+        ],
 
-    module: {
-        noParse: /\.elm$/,
-        rules: [
-            {
-                test: /\.elm$/,
-                exclude: [
-                    /elm-stuff/,
-                    /node_modules/
-                ],
-                use: {
-                    loader: 'elm-webpack-loader',
-                    options: {
-                        debug: true
-                    }
-                }
-            },
-            {
-                test: /\.p?css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
+        output: {
+            path: path.resolve('./build'),
+            filename: '[name]-[hash].js'
+        },
+
+        module: {
+            noParse: /\.elm$/,
+            rules: env.prod ? [
+                {
+                    test: /\.elm$/,
+                    exclude: [
+                        /elm-stuff/,
+                        /node_modules/
+                    ],
+                    use: {
+                        loader: 'elm-webpack-loader',
                         options: {
-                            importLoaders: 1
+                            optimize: true
+                        }
+                    }
+                },
+                {
+                    test: /\.p?css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader'
+                    ]
+                }
+            ] : [
+                    {
+                        test: /\.elm$/,
+                        exclude: [
+                            /elm-stuff/,
+                            /node_modules/
+                        ],
+                        use: {
+                            loader: 'elm-webpack-loader',
+                            options: {
+                                debug: true
+                            }
                         }
                     },
                     {
-                        loader: 'postcss-loader'
+                        test: /\.p?css$/,
+                        use: [
+                            'style-loader',
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    importLoaders: 1
+                                }
+                            },
+                            'postcss-loader'
+                        ]
                     }
                 ]
-            }
-        ]
-    },
+        },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve('./src/index.html'),
-            inject: 'body'
-        })
-    ],
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve('./src/index.html'),
+                inject: 'body',
+                minify: env.prod && {
+                    caseSensitive: true,
+                    collapseBooleanAttributes: true,
+                    collapseInlineTagWhitespace: true,
+                    collapseWhitespace: true,
+                    quoteCharacter: '"',
+                    removeAttributeQuotes: true,
+                    removeComments: true,
+                    removeEmptyAttributes: true,
+                    useShortDoctype: true
+                }
+            })
+        ].concat(env.prod ? [
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            })
+        ] : []),
 
-    mode: 'development'
+        mode: env.prod ? 'production' : 'development',
+
+        optimization: env.prod && {
+            minimizer: [
+                new UglifyJsPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: false
+                }),
+                new OptimizeCSSAssetsPlugin({})
+            ]
+        }
+    };
 };
